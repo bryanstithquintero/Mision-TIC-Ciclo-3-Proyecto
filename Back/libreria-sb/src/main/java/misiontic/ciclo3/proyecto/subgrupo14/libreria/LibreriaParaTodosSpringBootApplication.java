4455d4d4d86b9be8be2e4f0a659219cf8225cfc7
Bryan Quintero <bryanquinterog@gmail.com>
2022-09-05 20:48:49 -0500

    Instalacion spring boot en el proyecto

---
+++ Back/libreria-sb/src/main/java/misiontic/ciclo3/proyecto/subgrupo14/libreria/LibreriaParaTodosSpringBootApplication.java
@@ -0,0 +1,13 @@
+package misiontic.ciclo3.proyecto.subgrupo14.libreria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaParaTodosSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaParaTodosSpringBootApplication.class, args);
+	}
+
+}
---
+++ Back/libreria-sb/src/test/java/misiontic/ciclo3/proyecto/subgrupo14/libreria/LibreriaParaTodosSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package misiontic.ciclo3.proyecto.subgrupo14.libreria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaParaTodosSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
