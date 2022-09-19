151474871b997828e97b21f0a3418357d12823cc
Bryan Quintero <bryanquinterog@gmail.com>
2022-09-19 16:11:34 -0500

    Controlador y navegacion con este

---
+++ Back/libreria-sb/src/main/java/misiontic/ciclo3/proyecto/subgrupo14/libreria/controller/LibreriaController.java
@@ -0,0 +1,43 @@
+package misiontic.ciclo3.proyecto.subgrupo14.libreria.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Controller
+
+public class LibreriaController {
+    @GetMapping("/Contacto")
+    public String goToContact(Model model) {
+        return "Contacto";
+    }
+
+    @GetMapping("/DatosUsuario")
+    public String goToDatosUsuario(Model model) {
+        return "DatosUsuario";
+    }
+
+    @GetMapping("/Libro")
+    public String goToLibro(Model model) {
+        return "Libro";
+    }
+
+    @GetMapping("/listaLibros")
+    public String goToClistaLibros(Model model) {
+        return "listaLibros";
+    }
+
+    @GetMapping("/principal")
+    public String goToprincipal(Model model) {
+        return "principal";
+    }
+
+    @GetMapping("/RegistroLibros")
+    public String goToRegistroLibros(Model model) {
+        return "RegistroLibros";
+    }
+
+}
